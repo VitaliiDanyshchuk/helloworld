3a51e31a134e37236b9ac7238a2bb52b087797be
shimko23 <shimko2323@gmail.com>
2020-05-26 13:26:50 +0300

    First program/ Write to console 8 facts about yourself using primitive types.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-                System.out.println("Hello ");
-        System.out.println("test1");
 
+        byte age = 21;
+        short height = 170;
+        int currentSalary = 12000;
+        long minutesLived = 2147483648L;
+        float weight = 75.7f;
+        double metersPassed = 13324.31234;
+        boolean isMarried = false;
+        char signature = '@';
+
+        System.out.println("My age is: " + age);
+        System.out.println("My height is: " + height + " cm");
+        System.out.println("My Current salary is: " + currentSalary + " grivnas");
+        System.out.println("I lived for: " + minutesLived + " minutes");
+        System.out.println("My weight is: " + weight + " kg");
+        System.out.println("I passed: " + metersPassed + " meters");
+        System.out.println("I`m married?: " + isMarried );
+        System.out.println("My signature: " + signature);
         }
     }
 
