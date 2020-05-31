64d5fc2c639ae5273ef17a9c88710f1729a916c3
shimko23 <shimko2323@gmail.com>
2020-05-31 11:51:24 +0300

    First program/ Write to console 8 facts about yourself using primitive types. Second program/ Is palindrom? Lab work/ about Harry Potter * changed 31.05.2020/

--- src/com/company/Harry.java
+++ src/com/company/Harry.java
@@ -1,3 +1,11 @@
+/*
+ * Laboratory work № 1. Strings, arrays, methods.
+ *
+ * Version info 1
+ *
+ * Copyright Vitalii Danyshchuk
+
+ */
 package com.company;
 
 import org.w3c.dom.ls.LSOutput;
@@ -10,57 +18,65 @@ import java.util.Arrays;
 
 public class Harry {
     public static void main(String[] args) throws IOException {
-
+// get a harry.txt into string
         String text = new String(Files.readAllBytes(Paths.get(
                 "C:\\Users\\Vitaliy\\Desktop\\harry.txt")));
         String longestWord = "";
+// clear the text of punctuation
+        Integer[] hash = new Integer[text.length()];
         String clearText = text
-                .replaceAll("\\?","")
-                .replaceAll(",","")
-                .replaceAll("\\!","")
-                .replaceAll("\\.","")
-                .replaceAll("\"","")
-                .replaceAll("-","")
-                .replaceAll("\n","");
-
-        String [] words = clearText.split(" ");
+                .replaceAll("\\?", "")
+                .replaceAll(",", "")
+                .replaceAll("\\!", "")
+                .replaceAll("\\.", "")
+                .replaceAll("\"", "")
+                .replaceAll("-", "")
+                .replaceAll("\n", "");
 
 
-        for (int i = 0; i < words.length ; i++) {
-            if(words[i].length() >= longestWord.length()){
+//get array of words
+        String[] words = clearText.split(" ");
+// get array of lines
+        String[] lines = text.split("\n");
+
+// search the longest word
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() >= longestWord.length()) {
                 longestWord = words[i];
             }
         }
         System.out.println("The longest word is: '" + longestWord + "' " +
                 "with the " + longestWord.length() + " numbers in word ");
 
-        int harryWords=0;
-        for (int i = 0; i < words.length ; i++){
-            if(words[i].contains("Harry"))
-                harryWords++;
-        }
-        System.out.println("Word Harry" +" repeats "+harryWords+" times");
-
-        System.out.println(" hashCode: ");
-        for (int i = 0; i < words.length ; i++) {
-            System.out.println(words[i].hashCode());
-
+// serch count the LINES where the word "Harry" is met.
+        int harryLines = 0;
+        for (int i = 0; i < lines.length; i++) {
+            if (lines[i].contains("Harry"))
+                harryLines++;
         }
 
+        System.out.println("Lines with word \'Harry\'" + " repeats " + harryLines + " times");
+// create  hash array
         for (int i = 0; i < words.length; i++) {
+          hash[i]=words[i].hashCode();
+        }
+//Count the intersections of hashes.
+        for (int i = 0; i < hash.length; i++) {
 
-        int    count = 1;
+            int count = 1;
 
-            for (int j = i + 1; j < words.length; j++) {
-                if (words[i].equals(words[j])) {
+
+            for (int j = i + 1; j < hash.length; j++) {
+                if (hash[i].equals(hash[j])) {
                     count++;
-                    words[j] = "0";
+                    hash[j] = 0;
                 }
             }
 
-            if (count > 1 && words[i] != "0")
-                System.out.println("The word: "+ words[i]+ " repeats "+count+
-                        " times ");
-        }
+            if (count > 1 && hash[i] != 0)
+
+                 System.out.println("Hashcode: " + hash[i] + " repeats " +
+                       count + " times ");
         }
     }
+}
